4e24a51ee9f1874dcb3b9d9ccc8441955b99dc64
Syed Ukkashah <115150510+syedukkashah@users.noreply.github.com>
2025-05-06 23:32:07 +0500

    Create last.c

---
+++ practice/signals/last.c
@@ -0,0 +1,41 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+
+#define NUM_THREADS 4
+pthread_t threads[NUM_THREADS];
+
+void sigusr1_handler(int signum){
+    int i = gettid(), j = getppid(), k = getpid();
+    fprintf(stdout, "thread %u receievd SIGUSR1 signal (parent=%u)[pid=%u] {tid=%u} \n", pthread_self(), j,k,i);
+}
+
+void* thread_func(void* arg)
+{
+    while(1) sleep(1); //keep the thread alive
+    return NULL;
+}
+int main(int argc, char* argv[])
+{
+    signal(SIGUSR1, sigusr1_handler);
+    for(int i=0; i<NUM_THREADS; i++)
+    {
+        if(pthread_create(&threads[i],NULL, thread_func, NULL)!=0)
+            perror("pthread_create"); exit(EXIT_FAILURE);
+    }
+    int i = gettid(), j = getpid(), k = getppid();
+    fprintf(stdout, "parent process ID: %u, process ID: %u, main() thread id: %u\n", k, j, i);
+    fprintf(stdout, "thread IDs: 0=%u, 1=%u, 2=%u, 3=%u", threads[0], threads[1], threads[2], threads[3]);
+    kill(j,SIGUSR1);
+    pthread_kill(threads[2], SIGUSR1);
+
+    for(int i =0; i<NUM_THREADS; i++){
+        if(pthread_join(threads[i], NULL)!=0){
+            perror("pthread_join");exit(EXIT_FAILURE);
+        }
+    }
+    return 0;
+
+}
