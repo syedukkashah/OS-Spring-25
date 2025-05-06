e77a4dd535e84ce745c9edf02632617312ccd53a
Syed Ukkashah <115150510+syedukkashah@users.noreply.github.com>
2025-05-07 00:20:42 +0500

    Update last.c

--- practice/signals/last.c
+++ practice/signals/last.c
@@ -8,7 +8,7 @@
 pthread_t threads[NUM_THREADS];
 
 void sigusr1_handler(int signum){
-    int i = gettid(), j = getppid(), k = getpid();
+    int i = gettid(), j = getppid(), k = getpid(); // i = kernel thread id
     fprintf(stdout, "thread %u receievd SIGUSR1 signal (parent=%u)[pid=%u] {tid=%u} \n", pthread_self(), j,k,i);
 }
 
@@ -19,20 +19,20 @@ void* thread_func(void* arg)
 }
 int main(int argc, char* argv[])
 {
-    signal(SIGUSR1, sigusr1_handler);
+    signal(SIGUSR1, sigusr1_handler); //signal is registered by using singal func and passing signalnum and handler func as params
     for(int i=0; i<NUM_THREADS; i++)
     {
-        if(pthread_create(&threads[i],NULL, thread_func, NULL)!=0)
+        if(pthread_create(&threads[i],NULL, thread_func, NULL)!=0) //4 threads created
             perror("pthread_create"); exit(EXIT_FAILURE);
     }
     int i = gettid(), j = getpid(), k = getppid();
     fprintf(stdout, "parent process ID: %u, process ID: %u, main() thread id: %u\n", k, j, i);
     fprintf(stdout, "thread IDs: 0=%u, 1=%u, 2=%u, 3=%u", threads[0], threads[1], threads[2], threads[3]);
-    kill(j,SIGUSR1);
-    pthread_kill(threads[2], SIGUSR1);
+    kill(j,SIGUSR1); //sigusr1 sent to kernel process
+    pthread_kill(threads[2], SIGUSR1); //sigusr1 sent to 3rd thread 
 
     for(int i =0; i<NUM_THREADS; i++){
-        if(pthread_join(threads[i], NULL)!=0){
+        if(pthread_join(threads[i], NULL)!=0){ //threads are ran
             perror("pthread_join");exit(EXIT_FAILURE);
         }
     }
